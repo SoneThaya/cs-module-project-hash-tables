3d4ea3021020b397cfc0596158de99c0be406eb2
Sone <sthaya@gmail.com>
2020-08-05 16:25:34 -0700

    completed no dups, word count and expensive seq

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,7 +1,22 @@
 def no_dups(s):
     # Your code here
+    words = s.split()
+    count = 0
+    
+    d = {}
+    space = " "
+    
+    for word in words:
+        if word in d:
+            continue
+        else:
+            count += 1
+            d[word] = count
+    return space.join(d.keys())
+    
 
-
+    
+print(no_dups("hello hello"))
 
 if __name__ == "__main__":
     print(no_dups(""))
